7c86a7fe7032025eef77f0b298ed30388e113ffb
RostikHil <rostykepic101@gmail.com>
2022-01-28 19:51:52 +0200

    site
    Site

---
+++ nerds.js
@@ -0,0 +1,15 @@
+
+var link=document.querySelector(".login-link");
+popup=document.querySelector(".modal-login");
+close=popup.querySelector(".modal-close");
+link.addEventListener("click",function(e) {
+    e.preventDefault()
+    popup.classList.add("modal-show")
+}),
+close.addEventListener("click",function(e) {
+    e.preventDefault()
+    popup.classList.remove("modal-show")
+}),
+window.addEventListener("keydown",function(e) {
+    27===e.keyCode&&popup.classList.contains("modal-show")&&popup.classList.remove("modal-show")});
+
